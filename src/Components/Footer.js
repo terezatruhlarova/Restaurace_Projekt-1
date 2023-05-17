12da4243aab846b2d6f6eb59c3a1f3d0b1c8e2d3
Mistrbarta <AdamBarta12@seznam.cz>
2023-05-17 16:08:23 +0200

    Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "../Styles/Footer.css"
+import img from "../Img/Footer-img.png";
+import { FaFacebook, FaInstagram, FaSteam, FaTwitch } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
+      <footer className="footer">
+        <div className="footer-content">
+          <p>Majitel: Truhlářová Tereza</p>
+          <p>Kuchař: Bárta Adam</p>
+          <p>Rozvoz: Hovorka Ondřej</p>
+          <p>E-mail: papa-joy@gmail.com</p>
+          <p>Kontekt: +420 123 456 789</p>
+        </div>
+        <div className="footer-social">
+          <FaFacebook size="20px" className="footer-social-img" />
+          <FaInstagram size="20px" className="footer-social-img" />
+          <FaSteam size="20px" className="footer-social-img" />
+          <FaTwitch size="20px" className="footer-social-img" />
+        </div>
+        <div className="footer-content-img">
+          <img className="footer-img" src={img} width="320px" height="200px" alt=""/>
+        </div>
+      </footer>
   );
 }
 
