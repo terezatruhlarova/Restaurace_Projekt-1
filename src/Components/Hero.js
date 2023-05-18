d75ab39dcdc3b219e64989fdc6783ea28f0d1a63
MistrBarta <Adambarta12@seznam.cz>
2023-05-18 13:18:25 +0200

    footer responzivita

---
+++ src/Components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Styles/Hero.css";
+import img from "../Img/Hero-img.png";
+
+ function Hero(){
+    return(
+      <div>
+        
+      </div>
+    )
+}
+
+export default Hero;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,11 @@
 import "../Styles/Home.css"
 import React from "react";
+import Hero from "./Hero";
 
  function Home(){
     return(
       <div>
+        <Hero/>
         <p>Home</p>
       </div>
     )
